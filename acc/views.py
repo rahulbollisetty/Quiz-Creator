ff53c3b0ae78d7159846b3f8f4cd3295d76b8439
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-09-25 00:51:41 +0530

    main v-1

--- acc/views.py
+++ acc/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import NewUserForm,ProfileForm
 from django.contrib import messages
-from .models import Profile
 from django.contrib.auth.models import User
 
 # Create your views here.
